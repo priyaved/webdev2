44cb6289cf85b795edfa09358de110e46b43f84d
Priyaved <priyaved87@gmail.com>
2020-10-20 14:28:47 +0200

    updated services

---
+++ HTML/JavaScript/app.js
@@ -0,0 +1,10 @@
+document.write('Hello World!')
+
+var intecbrusseldiv=document.getElementById('intecbrussel');
+
+intecbrusseldiv.innerHTML=('This is cool');
+
+var intecbrussel = 'this is string'
+console.log(intecbrussel)
+
+
